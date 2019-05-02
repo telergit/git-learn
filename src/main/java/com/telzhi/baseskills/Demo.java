3d76cadbb8f86d5a916137aa053103e9c8e7570b
tja <teler1024@163.com>
2019-05-03 00:41:45 +0800

    create a new class demo

---
+++ src/main/java/com/telzhi/baseskills/Demo.java
@@ -0,0 +1,11 @@
+package com.telzhi.baseskills;
+
+/**
+ * Created by telerTJA on 2019/5/3.
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("this is the fist program of git study");
+    }
+}
